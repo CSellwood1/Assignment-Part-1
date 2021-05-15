f281eb3a958a149ccaa21d4d4a084278e537fd50
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-05-15 12:29:08 +0100

    Imported files and created folders for them. Added .mp3 to gitignore

--- importing bird sounds.R
+++ importing bird sounds.R
@@ -16,43 +16,58 @@ chaffinch_songs <- query_xc(qword = 'Fringilla coelebs cnt:"united kingdom" type
 
 yellowhammer_songs <- query_xc(qword = 'Emberiza citrinella cnt:"united kingdom" type:song len:5-25', download = FALSE)
 
-blackbird_songs
+robin_songs
 #shows lots of info including quality (A highest, E lowest), other sp heard in the clip etc
 #map samples
-map_xc(blackbird_songs, leaflet.map = TRUE)
+map_xc(robin_songs, leaflet.map = TRUE)
 
 #make folders for downloading audios
-dir.create(file.path("blackbird_songs"))
-dir.create(file.path("blackbird_alarm"))
-# Download the .MP3 files into two separate sub-folders
-query_xc(X = blackbird_songs, path="blackbird_songs")
-query_xc(X = blackbird_alarm, path="blackbird_alarm")
-#use this next bit of code to rename files to be either song or alarm files
+dir.create(file.path("robin_songs"))
+dir.create(file.path("chaffinch_songs"))
+dir.create(file.path("yellowhammer_songs"))
+# Download the .MP3 files into three separate sub-folders
+query_xc(X = robin_songs, path="robin_songs")
+query_xc(X = chaffinch_songs, path="chaffinch_songs")
+query_xc(X = yellowhammer_songs, path="yellowhammer_songs")
 
+#use this next bit of code to rename files for each species
 
-old_files <- list.files("blackbird_songs", full.names=TRUE)
+old_files <- list.files("robin_songs", full.names=TRUE)
 new_files <- NULL
 for(file in 1:length(old_files)){
   curr_file <- str_split(old_files[file], "-")
-  new_name <- str_c(c(curr_file[[1]][1:2], "-song_", curr_file[[1]][3]), collapse="")
+  new_name <- str_c(c(curr_file[[1]][1:2], "-robin_", curr_file[[1]][3]), collapse="")
   new_files <- c(new_files, new_name)
 }
 file.rename(old_files, new_files)
-#do the same for the alarm call files
-old_files <- list.files("blackbird_alarm", full.names=TRUE)
+
+#do the same for the chaffinch song files
+old_files <- list.files("chaffinch_songs", full.names=TRUE)
 new_files <- NULL
 for(file in 1:length(old_files)){
   curr_file <- str_split(old_files[file], "-")
-  new_name <- str_c(c(curr_file[[1]][1:2], "-alarm_", curr_file[[1]][3]), collapse="")
+  new_name <- str_c(c(curr_file[[1]][1:2], "-chaffinch_", curr_file[[1]][3]), collapse="")
   new_files <- c(new_files, new_name)
 }
 file.rename(old_files, new_files)
-#put these files in a new subfolder in the project called blackbird_audio
-dir.create(file.path("blackbird_audio"))
-file.copy(from=paste0("blackbird_songs/",list.files("blackbird_songs")),
-          to="blackbird_audio")
-file.copy(from=paste0("blackbird_alarm/",list.files("blackbird_alarm")),
-          to="blackbird_audio")
+#finally for yellowhammer files
+old_files <- list.files("yellowhammer_songs", full.names=TRUE)
+new_files <- NULL
+for(file in 1:length(old_files)){
+  curr_file <- str_split(old_files[file], "-")
+  new_name <- str_c(c(curr_file[[1]][1:2], "-yellowhammer_", curr_file[[1]][3]), collapse="")
+  new_files <- c(new_files, new_name)
+}
+file.rename(old_files, new_files)
+
+#put these files in a new subfolder in the project called bird_audio
+dir.create(file.path("bird_audio"))
+file.copy(from=paste0("robin_songs/",list.files("robin_songs")),
+          to="bird_audio")
+file.copy(from=paste0("chaffinch_songs/",list.files("chaffinch_songs")),
+          to="bird_audio")
+file.copy(from=paste0("yellowhammer_songs/",list.files("yellowhammer_songs")),
+          to="bird_audio")
 #use mp32wav func in warbleR to convert these mp3s to .wav files
 mp32wav(path="blackbird_audio", dest.path="blackbird_audio")
 unwanted_mp3 <- dir(path="blackbird_audio", pattern="*.mp3")
